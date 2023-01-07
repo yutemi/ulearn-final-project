846e981b47cb95b837b83cf1e1e27cc1cbb21c7d
yutemi <sayshade@yandex.ru>
2023-01-07 06:38:12 +0300

    final solution

--- src/ProductsInWorld/Parser.java
+++ src/ProductsInWorld/Parser.java
@@ -13,9 +13,14 @@ public class Parser {
         try (CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(1).build()){
             List<String[]> allData = csvReader.readAll();
             for (String[] row : allData) {
+                for (String cell : row){
+                    System.out.print(cell + ", ");
+                }
+                System.out.println();
                 Products products = new Products(row);
                 SalesOfProductsList.add(products);
             }
+
         }
         catch (Exception e) {
             e.printStackTrace();
